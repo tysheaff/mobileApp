411f6f909f7aff61ae6b5e1b7416856bf54dddef
Ahmad Mazaal <61668890+AhmadMazaal@users.noreply.github.com>
2021-06-21 11:35:21 +0300

    Added Copy Text Option to ActionSheet (#36)

--- src/components/post/postOptions.components.tsx
+++ src/components/post/postOptions.components.tsx
@@ -37,86 +37,90 @@ export class PostOptionsComponent extends React.Component<Props> {
     }
 
     private showNotOwnPostOptions() {
-        const options = ['Copy Link', 'Report', 'Block User', 'Cancel'];
+        const options = ['Copy Link', 'Copy Text', 'Report', 'Block User', 'Cancel'];
 
         const callback = async (p_optionIndex: number) => {
-            if (p_optionIndex === 0) {
-                this.copyToClipBoard();
-            } else if (p_optionIndex === 1) {
-                Linking.openURL(`https://report.bitclout.com/?ReporterPublicKey=${globals.user.publicKey}&PostHash=${this.props.post.PostHashHex}`);
-            } else if (p_optionIndex === 2) {
-                const jwt = await signing.signJWT();
+            switch (p_optionIndex) {
+                case 0:
+                    return this.copyToClipBoard(true);
+                case 1:
+                    return this.copyToClipBoard(false);
+                case 2:
+                    return Linking.openURL(`https://report.bitclout.com/?ReporterPublicKey=${globals.user.publicKey}&PostHash=${this.props.post.PostHashHex}`);
+                case 3:
+                    const jwt = await signing.signJWT();
 
-                api.blockUser(globals.user.publicKey, this.props.post.ProfileEntryResponse.PublicKeyBase58Check, jwt as string, false).then(
-                    () => this.props.navigation.navigate(
-                        'Home',
-                        {
-                            blockedUser: this.props.post.ProfileEntryResponse.PublicKeyBase58Check
-                        }
-                    )
-                ).catch(() => Alert.alert('Error', 'Something went wrong! Please try again.'));
+                    api.blockUser(globals.user.publicKey, this.props.post.ProfileEntryResponse.PublicKeyBase58Check, jwt as string, false).then(
+                        () => this.props.navigation.navigate(
+                            'Home',
+                            {
+                                blockedUser: this.props.post.ProfileEntryResponse.PublicKeyBase58Check
+                            }
+                        )
+                    ).catch(() => Alert.alert('Error', 'Something went wrong! Please try again.'));
             }
-
         };
 
         eventManager.dispatchEvent(
             EventType.ToggleActionSheet,
             {
                 visible: true,
-                config: { options, callback, destructiveButtonIndex: [1, 2] }
+                config: { options, callback, destructiveButtonIndex: [2, 3] }
             }
         );
     }
 
 
     private showOwnPostOptions() {
-        const options = ['Copy Link', 'Edit', 'Delete Post', 'Cancel'];
+        const options = ['Copy Link', 'Copy Text', 'Edit', 'Delete Post', 'Cancel'];
 
         const callback = (p_optionIndex: number) => {
-            if (p_optionIndex === 0) {
-                this.copyToClipBoard();
-            } else if (p_optionIndex === 1) {
-                if (this.props.post.Body || this.props.post.ImageURLs?.length > 0) {
-                    this.props.navigation.navigate(
-                        'CreatePost',
-                        {
+            switch (p_optionIndex) {
+                case 0:
+                    return this.copyToClipBoard(true);
+                case 1:
+                    return this.copyToClipBoard(false);
+                case 2:
+                    if (this.props.post.Body || this.props.post.ImageURLs?.length > 0) {
+                        this.props.navigation.navigate(
+                            'CreatePost',
+                            {
 
-                            editPost: true,
-                            editedPost: this.props.post
-                        }
-                    );
-                } else {
-                    Alert.alert('Sorry!', 'You cannot edit a reclout, if it does not include a quote.');
-                }
-            }
-            else if (p_optionIndex === 2) {
-                api.hidePost(
-                    globals.user.publicKey,
-                    this.props.post.PostHashHex,
-                    this.props.post.Body,
-                    this.props.post.ImageURLs,
-                    this.props.post.RecloutedPostEntryResponse?.PostHashHex
-                ).then(
-                    async p_response => {
-                        const transactionHex = p_response.TransactionHex;
-
-                        const signedTransactionHex = await signing.signTransaction(transactionHex);
-                        await api.submitTransaction(signedTransactionHex as string);
-
-                        if (this.props.route.name === 'Home' || this.props.route.name === 'Profile') {
-                            Alert.alert('Success', 'Your post was deleted successfully.');
-                            this.props.navigation.navigate(
-                                this.props.route.name,
-                                {
-                                    deletedPost: this.props.post.PostHashHex
-                                }
-                            );
-
-                        } else {
-                            Alert.alert('Success', 'Your post was deleted successfully. Please reload the screen to see this change.');
-                        }
+                                editPost: true,
+                                editedPost: this.props.post
+                            }
+                        );
+                    } else {
+                        Alert.alert('Sorry!', 'You cannot edit a reclout, if it does not include a quote.');
                     }
-                ).catch(p_error => globals.defaultHandleError(p_error));
+                case 3:
+                    api.hidePost(
+                        globals.user.publicKey,
+                        this.props.post.PostHashHex,
+                        this.props.post.Body,
+                        this.props.post.ImageURLs,
+                        this.props.post.RecloutedPostEntryResponse?.PostHashHex
+                    ).then(
+                        async p_response => {
+                            const transactionHex = p_response.TransactionHex;
+
+                            const signedTransactionHex = await signing.signTransaction(transactionHex);
+                            await api.submitTransaction(signedTransactionHex as string);
+
+                            if (this.props.route.name === 'Home' || this.props.route.name === 'Profile') {
+                                Alert.alert('Success', 'Your post was deleted successfully.');
+                                this.props.navigation.navigate(
+                                    this.props.route.name,
+                                    {
+                                        deletedPost: this.props.post.PostHashHex
+                                    }
+                                );
+
+                            } else {
+                                Alert.alert('Success', 'Your post was deleted successfully. Please reload the screen to see this change.');
+                            }
+                        }
+                    ).catch(p_error => globals.defaultHandleError(p_error));
             }
         }
 
@@ -124,19 +128,16 @@ export class PostOptionsComponent extends React.Component<Props> {
             EventType.ToggleActionSheet,
             {
                 visible: true,
-                config: { options, callback, destructiveButtonIndex: [2] }
+                config: { options, callback, destructiveButtonIndex: [3] }
             }
         );
     }
 
-    private copyToClipBoard() {
-        const postLink = 'https://bitclout.com/posts/' + this.props.post.PostHashHex;
+    private copyToClipBoard(p_isLink: boolean) {
+        let text: string = p_isLink ? 'Link copied to clipboard' : 'Text copied to clipboard';
+        const postLink = p_isLink ? `https://bitclout.com/posts/${this.props.post.PostHashHex}` : this.props.post.Body
         Clipboard.setString(postLink);
-        snackbar.showSnackBar(
-            {
-                text: 'Link copied to clipboard.'
-            }
-        );
+        snackbar.showSnackBar({ text });
     }
 
     render() {
